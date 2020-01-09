49f428defe4367b51daf9ff93b563029c6ea110e
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2020-01-09 10:43:59 +0100

    Configuration files.

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,43 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.persistence.EntityManagerFactory;
+import java.lang.annotation.Annotation;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+public class AppConfig implements EnableWebMvc {
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("springCmsPersistenceUnit");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager =
+                new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+
+    /**
+     * Returns the annotation type of this annotation.
+     *
+     * @return the annotation type of this annotation
+     */
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
+}
---
+++ src/main/java/pl/coderslab/entity/Category.java
@@ -0,0 +1,30 @@
+package pl.coderslab.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+
+
+}
